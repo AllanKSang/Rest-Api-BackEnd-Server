c3f11a516df6a5ffcf38df58bd380f5a83c8e9d2
Allan Kibet <87029508+allantuiya@users.noreply.github.com>
2023-10-17 21:25:55 +0300

    I added the bodyParser that reads urlencoded and json in client requests. I also added headers to address CRS errors.

--- app.js
+++ app.js
@@ -6,8 +6,24 @@ const bodyParser = require('body-parser');
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
-//For logging requests on the console
-app.use(morgan('dev'));
+app.use(morgan('dev'));//For logging requests on the console
+
+app.use(bodyParser.urlencoded({extended: false})); //For accessing the url content(non-alphanumeric) wrapped in client requests.
+app.use(bodyParser.json()); //For accessing Json formatted data in client requests.
+
+//To address CORS errors in the API, i.e 'error': 'No Access-Control-Allow-Origin header is present'
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); /*The asterisk(*) argument gives access to any origin. It can be replaced by:
+    'http://blacknblue.inc' to restrict access to that domain only*/
+    res.header('Access-Control-Allow-Headers', 'Origin,X-Requested-With, Content-Type, Accept, Authorization');/*
+    For attaching all the headers that could come with the client request. Again, a simple * could do here*/
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, PATCH');
+        return res.status(200).json({});
+    }
+    next();
+});
+
 
 //Routes which should handle requests
 app.use('/products', productRoutes);
@@ -22,7 +38,8 @@ app.use((req, res, next) => {
 
 //Error handling for errors not accounted for by 404
 app.use((error, req, res, next) => {
-    res.status(error.status || 500).json({
+    res.status(error.status || 500);
+    res.json({
         error: {
             message: error.message
         }
